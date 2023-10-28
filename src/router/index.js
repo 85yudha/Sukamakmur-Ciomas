ab3ea9a114754882c6f7c6b2b6609ec94a291aa8
Irfan yudha pratama <144505847+85yudha@users.noreply.github.com>
2023-10-28 16:19:14 +0700

    Update perdes index

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import MapView from "../views/MapView.vue";
+import PerdesView from "../views/PerdesView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +11,12 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+
+    {
+      path: "/",
+      name: "Perdes",
+      component: PerdesView,
     },
 
     {
