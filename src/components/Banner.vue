78d32e791c2c0f114ea9a850ad7fcb4af93db199
Irfan yudha pratama <144505847+85yudha@users.noreply.github.com>
2023-10-28 15:29:54 +0700

    Update youtube

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -16,7 +16,7 @@
       <iframe
         width="480"
         height="260"
-        src="https://www.youtube.com/embed/sgekBZWvw1g?si=FuzxQ1pEq1HGYh1K"
+        src="https://https://www.youtube.com/embed/nyUYs7l0_Mc?si=O7LuEYKKqE4LEf2V"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
