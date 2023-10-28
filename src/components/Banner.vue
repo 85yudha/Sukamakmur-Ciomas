2e359b6e0bf4be9e7f86c80ca8c23315fe22ac8d
Irfan yudha pratama <144505847+85yudha@users.noreply.github.com>
2023-10-28 14:09:16 +0700

    Update nama desa

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -8,8 +8,9 @@
     <div class="ml-10 flex flex-col justify-center">
       <p class="text-white text-2xl mb-2">Profile Desa</p>
       <!-- Sesuaikan nama desa masing-masing -->
-      <p class="text-white text-3xl">Desa Contoh Tugas akhir</p>
-      <p class="text-white text-3xl">Kab. Bogor</p>
+      <p class="text-white text-3xl">Desa Sukamakmur</p>
+      <p class="text-white text-3xl">Kecamatan Ciomas</p>
+      <p class="text-white text-3xl">Kabupaten Bogor</p>
     </div>
     <div class="grid place-items-center ml-auto mr-20">
       <iframe
