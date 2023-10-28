b838f5f05d2a96c1dd068ccd3e04d9f0c9ebcf70
Irfan yudha pratama <144505847+85yudha@users.noreply.github.com>
2023-10-28 16:29:18 +0700

    Update Perdes kita

--- src/views/PerdesView.vue
+++ src/views/PerdesView.vue
@@ -7,11 +7,95 @@ import Sidebar from "../components/Sidebar.vue";
 <template>
   <Header />
   <Banner />
+
   <main class="flex">
     <Sidebar />
+
     <div class="flex-1 p-10">
-      <p class="text-3xl font-semibold my-4">Daftar Peraturan Desa</p>
-      <img src="@/assets/Stuktur-Desa-Sukamakmur.png" alt="" />
+      <p class="text-3xl font-semibold">Daftar Peraturan Desa</p>
+      <p class="text-lg font-semibold">Visi</p>
+      <p>
+        “Mewujudkan Integritas Desa Mandiri berbasis Partisipasi Aktif Masyarakat”
+      </p>
+      <p class="text-lg font-semibold">Misi</p>
+      <ul>
+        <li>
+          1. Meningkatkan Tata Kelola Pemerintahan yang Cepat, Efektif, Ramah, Informatif dan Akuntabel (CERIA);
+          
+          
+        </li>
+        <li>
+          2. Meningkatkan Partisipasi Masyarakat dalam Perencanaan dan Pelaksanaan Pembangunan Desa;
+          
+          
+        </li>
+        <li>
+          3. Meningkatkan Sarana dan Prasarana Pendidikan, Kesehatan, dan Infrastruktur Desa;
+          
+          
+        </li>
+        <li>
+          4. Mewujudkan Tata Kelola Lingkungan yang Aman, Sehat dan Bersih;
+          
+          
+          
+        </li>
+        <li>
+          5. Mewujudkan Kehidupan Masyarakat yang Harmonis dan Religius;
+          
+          
+        </li>
+        <li>
+          6. Menumbuhkembangkan Potensi Ekonomi Lokal Masyarakat Desa;
+          
+          
+          
+        </li>
+        <li>
+          7. Meningkatkan Pendapatan Asli Desa melalui BUMDES.
+          
+          
+        </li>
+        <li>
+      <p class="text-lg font-semibold">Nila-nilai</p>    
+          Nilai-nilai yang akan dikembangkan dalam melaksanakan visi dan misi Kepala Desa adalah pembangunan yang dilaksanakan  di Desa Sukamakmur Kecamatan Ciomas Kabupaten Bogor dilaksanakan dengan berkeadilan dan tepat sasaran, perlu di operasionalisasi dan dilaksanakan dengan cara sebagai berikut :
+        </li>
+        <li>
+          1. Tata kelola Pemerintahan yang bersih, transparan dan akuntabel yang di wujudkan dengan pengelola keuangan yang baik, pelaksanaan
+             pembangunan yang sesuai dengan ketentuan perundang-undangan dan disiplin aparatur pemerintahan Desa dalam setiap tahapan pelaksanaan 
+             pembangunan.
+        </li>
+        <li> 
+          2. Pembangunan yang berkeadilan, diwujudkan dengan memperhatikan pemerataan pembangunan pada masing-masing wilayah RT / RW / lingkungan
+             permukiman, persawahan, dan lingkungan lainnya yang berhubungan dengan kesejahteraan masyarakat.
+        </li>
+        <li> 
+          3. Pembangunan Ekonomi yang berpihak kepada warga miskin, operasionalisasinya adalah keberpihakan pengelolaan anggaran yang pro terhadap
+             pengentasan kemiskinan dan kelompok warga miskin.
+        </li>
+        <li>
+          4. Pemanfaatan potensi desa yang sebesar-besarnya untuk kemakmuran warga Desa Sukamakmur, dengan memanfaatkan potensi sumberdaya manusia
+             warga Desa Sukamakmur dengan meminimalisir oligarki dalam pemanfaatan potensi Desa dimaksud.
+        </li>
+        <li>
+          5. CEPAT : Pelayanan yang cepat dalam menangani kebutuhan masyarakat  seperti halnya bila mana ada kekurangan persyaratan akan dibantu
+             semaksimal mungkin.
+        </li>
+        <li>
+          6. EFEKTIF : Singkat waktu, yang menjadikan masyarakat tidak perlu menunggu waktu yang lama, karena pemerintah desa sendiri mengerti akan
+             pentingnya waktu yang akan dikorbankan masyarakat.
+        </li>
+        <li>
+          7. RAMAH : Dengan murah senyum serta bahasa yang halus masyarakat akan lebih nyaman  saat komunikasi terkait hal hal yang dibutuhkan.
+        </li>
+        <li>
+          8. INFORMATIF : Memberikan penjelasan terlebih  dahulu sesuai kebutuhan yang di perlukan, jadi masyarakat lebih mengerti  apa saja persyaratan
+             yang di perlukan.
+        </li>
+        <li>
+          9. AKUNTABEL : Dapat di pertanggung jawabkan segala informasi yang diberikan kepada masyarakat.
+        </li>
+      </ul>
     </div>
   </main>
 </template>
